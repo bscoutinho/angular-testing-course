8891101247b66ec6329dd11905db54939fbb0990
DESKTOP-HVU60H0\Bruno Coutinho <bscoutinho2012@gmail.com>
2021-04-13 20:23:44 +0100

    first commit

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,44 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('CalculatorService', () => {
+
+    let calculator: CalculatorService,
+        loggerSpy: any
+
+    beforeEach( () => {
+
+        console.log("Calling before")
+
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"])
+        
+        calculator = new CalculatorService(loggerSpy)
+
+    })
+
+
+    it('should add two numbers', () => {
+
+        console.log("add test")
+
+        const result = calculator.add(2, 2)
+
+        expect(result).toBe(4)
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1)
+
+    });
+
+    it('should subtract two numbers', () => {
+
+        console.log("add test")
+
+        const result = calculator.subtract(2, 2)
+
+        expect(result).toBe(0, "unexpected result")
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1)
+
+    });
+
+});
